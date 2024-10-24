b524f6eaaf4cff00303b533b902ed87f005269d3
srffph <pedro.henripereira23@gmail.com>
2024-10-24 08:00:58 -0300

    Add files via upload

--- js/principal.js
+++ js/principal.js
@@ -1,5 +1,14 @@
-console.log("Fui carregado de um arquivo externo")
-
 var titulo = document.querySelector(".titulo");
-
 titulo.textContent = "Aparecida Nutricionista";
+
+var paciente = document.querySelector("#primeiro-paciente");
+
+var tdPeso = paciente.querySelector(".info-peso");
+var peso = tdPeso.textContent;
+
+var tdAlura = paiente.querySelector(".info-altura");
+var altura = tdAlura.textContent;
+
+var imc = peso / altura * altura; // 100 / 2.0 * 2.0 = / 4 =>>>>> 25
+
+console.log(imc);
